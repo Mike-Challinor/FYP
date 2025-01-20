28facc8bcd468f481952a6d69129ca4d1b906cd2
Michael Challinor <michael.challinor@live.com>
2025-01-20 14:00:34 +0000

    Updates to player
    Added health component
    Added attack functionality
    Created projectile prefab
    Handled spawning the projectile

--- Assets/Scripts/Player/Player_Controller.cs
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -1,22 +1,63 @@
 using UnityEngine;
+using System.Collections;
 
 public class Player_Controller : MonoBehaviour
 {
     [SerializeField] private GameObject m_playerCam;
     [SerializeField] private GameObject m_player;
+    [SerializeField] private bool m_isAttacking;
+    [SerializeField] private float m_attackTimer = 0.2f;
+    [SerializeField] private GameObject m_projectilePrefab;
+    [SerializeField] private GameObject m_firePoint;
+
+    private float m_maxHealth = 100.0f;
+    private Health_Component m_healthComponent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Initialise player camera
         m_playerCam = GameObject.FindGameObjectWithTag("MainCamera"); // Find the main camera game object
         m_playerCam.transform.SetParent(m_player.transform); // Set the player as the cameras new parent
-        m_playerCam.transform.localPosition = new Vector3(0, 0, -11); ; // Reset local position
+        m_playerCam.transform.localPosition = new Vector3(0, 0, -11); ; // Reset locallocal position
 
+        //Initialise health component
+        m_healthComponent = GetComponent<Health_Component>();
+        m_healthComponent.InitHealth(m_maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    public void InitiateAttack()
+    {
+        m_isAttacking = true;
+        StartCoroutine(Attack());
+    }
+
+    private IEnumerator Attack()
+    {
+        while(m_isAttacking)
+        {
+            // Spawn projectile here
+            GameObject projectile = Instantiate(m_projectilePrefab, m_firePoint.transform.position, m_firePoint.transform.rotation);
+
+            // Wait for attack timer before spawning another projectile
+            yield return StartCoroutine(AttackTimer());
+        }
         
     }
+
+    private IEnumerator AttackTimer()
+    {
+        yield return new WaitForSeconds(m_attackTimer);
+    }
+
+    public void EndAttack()
+    {
+        m_isAttacking = false;
+    }    
 }
--- Assets/Scripts/Player/Player_Input.cs
+++ Assets/Scripts/Player/Player_Input.cs
@@ -7,9 +7,12 @@ public class Player_Input : MonoBehaviour
     private Vector2 m_moveInput;
     private SpriteRenderer m_playerSprite;
     private Rigidbody2D m_RB;
+    private Player_Controller m_playerController;
+    private Vector3 m_firePointLocalPosition;
 
     [SerializeField] private float m_moveSpeed = 5f;
     [SerializeField] private bool m_isFacingRight = true;
+    [SerializeField] private GameObject m_firePoint;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +20,8 @@ public class Player_Input : MonoBehaviour
     {
         m_RB = GetComponent<Rigidbody2D>();
         m_playerSprite = GetComponent<SpriteRenderer>();
+        m_playerController = GetComponent<Player_Controller>();
+        m_firePointLocalPosition = m_firePoint.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -25,7 +30,7 @@ public class Player_Input : MonoBehaviour
         MovePlayer();
     }
 
-    protected virtual void OnEnable()
+    private void OnEnable()
     {
         m_playerActions = new PlayerInputActions();
 
@@ -36,12 +41,24 @@ public class Player_Input : MonoBehaviour
         // Stop movement on release
         m_playerActions.Player.Move.canceled += ctx => m_moveInput = Vector2.zero;
 
+        // Initiate Attack when pressed
+        m_playerActions.Player.Attack.performed += InitiateAttack;
+        // Stop attacking on release
+        m_playerActions.Player.Attack.canceled += EndAttack;
     }
 
-    protected virtual void OnDisable()
+    private void OnDisable()
     {
+        // Unsub from player movement
         m_playerActions.Player.Move.performed -= ctx => m_moveInput = ctx.ReadValue<Vector2>();
         m_playerActions.Player.Move.canceled -= ctx => m_moveInput = Vector2.zero;
+
+        // Unsub from player attack
+        // Unsub from player attack
+        m_playerActions.Player.Attack.performed -= InitiateAttack;
+        m_playerActions.Player.Attack.canceled -= EndAttack;
+
+        // Disable input
         m_playerActions.Disable();
     }
 
@@ -50,7 +67,7 @@ public class Player_Input : MonoBehaviour
         //Check to see if moving left or right
         if ((m_moveInput.x < 0 && m_isFacingRight) || (m_moveInput.x > 0 && !m_isFacingRight))
         {
-            FlipSpriteRpc();
+            FlipSprite();
         }
 
         m_RB.linearVelocity = m_moveInput * m_moveSpeed;
@@ -58,10 +75,35 @@ public class Player_Input : MonoBehaviour
 
     }
 
-    protected virtual void FlipSpriteRpc()
+    private void InitiateAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Initiate Attack!");
+        m_playerController.InitiateAttack();
+    }
+
+    private void EndAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("End Attack!");
+        m_playerController.EndAttack();
+    }
+
+    private void FlipSprite()
     {
         m_isFacingRight = !m_isFacingRight;
         m_playerSprite.flipX = !m_playerSprite.flipX;
+
+        if (m_isFacingRight)
+        {
+            // Set the firepoint to right of the player
+            m_firePoint.transform.localPosition = new Vector3(Mathf.Abs(m_firePointLocalPosition.x), m_firePointLocalPosition.y, 0);
+        }
+
+        else
+        {
+            // Set the firepoint to right of the player
+            m_firePoint.transform.localPosition = new Vector3(-Mathf.Abs(m_firePointLocalPosition.x), m_firePointLocalPosition.y, 0);
+        }
+
     }
 
 }
